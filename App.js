feaee729f7ceace01988e47d862fe47cff5b761d
abdulumar1111 <abdulumar1111@gmail.com>
2023-11-21 22:34:40 +0530

    In this commit learn view text button

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
-  return(
-    <View>
-      <Text>Hello World</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello Zeeshan Ansari</Text>
+      <Button title="Click Here" color={'red'} />
     </View>
-  )
-}
+  );
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f4f4f4',
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#333333',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+});
