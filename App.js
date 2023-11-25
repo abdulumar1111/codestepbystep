a25dc53a6d8934075615f47b0ee49135a73a39e9
abdulumar1111 <abdulumar1111@gmail.com>
2023-11-25 10:18:09 +0530

    In this commit state in react native

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
-  return(
-    <View>
-      <Text>Hello World</Text>
+  const [name, setName] = useState('Zeeshan Ansari');
+  const handlePress = () => {
+    setName('Asba Ansari');
+  };
+  return (
+    <View style={Styles.container}>
+      <Text style={Styles.title}>State in React Native </Text>
+      <Text style={Styles.title}>Hello {name}</Text>
+      <Button title="Click Here" onPress={handlePress} />
     </View>
-  )
-}
+  );
+};
 
 export default App;
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f4f4f4',
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+  },
+  title: {
+    color: '#333',
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 8,
+  },
+});
