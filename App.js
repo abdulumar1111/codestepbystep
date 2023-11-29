0cb3558d81e03cb1abf9100cedadac4252ae2ea3
abdulumar1111 <abdulumar1111@gmail.com>
2023-11-29 23:06:08 +0530

    In this commit responsive screen

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
-  return(
-    <View>
-      <Text>Hello World</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.box1}>
+        <View style={styles.subBox1}>
+          <Text>Sub Box 1</Text>
+        </View>
+        <View style={styles.subBox2}>
+          <Text>Sub Box 2</Text>
+        </View>
+        <View style={styles.subBox3}>
+          <Text>Sub Box 3</Text>
+        </View>
+      </View>
+
+      <View style={styles.box2}>
+        <Text>Box 2</Text>
+      </View>
+
+      <View style={styles.box3}>
+        <Text>Box 3</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  box1: {
+    flex: 2,
+    backgroundColor: '#ff6b6b',
+    flexDirection: 'row',
+  },
+  box2: {
+    flex: 1,
+    backgroundColor: '#1dd1a1',
+  },
+  box3: {
+    flex: 1,
+    backgroundColor: '#ff9f43',
+  },
+  subBox1: {
+    flex: 1,
+    backgroundColor: '#feca57',
+  },
+  subBox2: {
+    flex: 1,
+    backgroundColor: '#5f27cd',
+  },
+  subBox3: {
+    flex: 1,
+    backgroundColor: '#576574',
+  },
+});
